e2f2f8ec42aff9cc25f1acaf62cbba7a9aa23bc8
Tulxy <tomas.vylimec@gmail.com>
2026-04-16 15:34:02 +0200

    Update layout to include additional CSS and JS assets for tags and profile sharing features

--- SkolniTinder/resources/views/components/layout.blade.php
+++ SkolniTinder/resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
 
     <title>{{ isset($title) ? $title . ' - EduVote' : 'EduVote' }}</title>
 
-    @vite(['resources/css/app.css', 'resources/css/tags.css', 'resources/js/app.js', 'resources/js/tags.js', 'resources/js/clipboard.js', 'resources/js/profile-share.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
